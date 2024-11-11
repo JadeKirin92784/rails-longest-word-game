5296f727d69938e20a0712c5ef1e75c584c002c0
mason kamolpechara <mason.kamolpechara@gmail.com>
2024-11-11 16:18:55 +0900

    works but ugly

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,43 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @letters = (0...10).map { (65 + rand(26)).chr}
+    @start_time = Time.now
+  end
+
+  def score
+    @end_time = Time.now
+    @guess = params[:guess]
+    @letters = params[:letters].split
+    @start_time = Time.new(params[:start_time])
+
+    received_json = URI.parse("https://dictionary.lewagon.com/#{@guess}").read
+    response = JSON.parse(received_json)
+    @result = {}
+
+    if pass_grid_check?(@guess, @letters)
+      @result = hit_api(response, @end_time - @start_time, @guess)
+    else
+      @result = { time: 0, score: 0, message: "Sorry, but #{@guess} can't be built out of #{@letters}" }
+    end
+
+  end
+
+  def pass_grid_check?(attempt, grid)
+    attempt_chars = attempt.downcase.chars
+    grid_chars = grid.join.downcase.chars
+    return attempt_chars.all? { |char| grid_chars.count(char) >= attempt_chars.count(char) }
+  end
+
+  def hit_api(response, time_elasped, attempt)
+    if response["found"]
+      result = { time: time_elasped, score: attempt.length / time_elasped, message: "Congratulations! #{@guess} is a value English word!" }
+    else
+      result = { time: time_elasped, score: 0, message: "Sorry, but #{@guess} does not seem to be a valid English word..." }
+    end
+    return result
+  end
+
+end
